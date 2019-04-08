883c28390023168a603fbf1a39c731e706766e2c
aditi <aditi.gautam@hashedin.com>
2019-04-08 12:05:33 +0530

    Added html css and tunnel for Redis

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('html'))
 
-class HelloWorld(object):
+class DataDisplay(object):
 
     def get_csv(self):
         url = "https://www.bseindia.com/markets/MarketInfo/BhavCopy.aspx"
@@ -24,18 +24,32 @@ class HelloWorld(object):
             links.append(link.get('href'))
         resp = urllib.request.urlopen(links[1])
         zipfile = ZipFile(BytesIO(resp.read()))
-
+        red = redis.StrictRedis(host= 0.tcp.ngrok.io, port=15673,charset="utf-8", decode_responses=True)
         df = pd.read_csv((zipfile.open(zipfile.namelist()[0])))
         new = df.filter(['SC_CODE', 'SC_NAME', 'OPEN', 'HIGH', 'LOW', 'CLOSE'], axis=1)
-        key = 0
+        key=0
         for i in json.loads(new.to_json(orient='records')):
-            red.hmset(key, i)
+            red.hmset("name:"+i.get("SC_NAME")+":"+str(key), i)
+            key+=1
 
-        red = redis.StrictRedis(charset="utf-8", decode_responses=True)
         cursor, possible_keys = red.scan(count=10)
 
         list = [red.hgetall(items) for items in possible_keys]
         return list
+
+    @cherrypy.expose
+    def search(self, search=''):
+        red = redis.StrictRedis(charset="utf-8", decode_responses=True)
+        match_pattern="name:*"+search+"*"
+        tmpl = env.get_template('index.html')
+        result = []
+        cur, keys = red.scan(cursor=0, match=match_pattern, count=2)
+        result.extend(keys)
+        while cur != 0:
+            cur, keys = red.scan(cursor=cur, match=match_pattern, count=2)
+            result.extend(keys)
+        data_to_show= [red.hgetall(items) for items in result]
+        return tmpl.render(data=data_to_show)
 
     @cherrypy.expose
     def index(self):
@@ -55,4 +69,4 @@ config = {
     }
 }
 
-cherrypy.quickstart(HelloWorld(), '/', config=config)
+cherrypy.quickstart(DataDisplay(), '/', config=config)
